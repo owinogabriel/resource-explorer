b148c269f191bad123e7b26acea123ac51e3efbe
owinogabriel <g73346920@gmail.com>
2025-08-11 14:37:22 +0300

    chore(ui): remove empty SelectProps interface, use React.SelectHTMLAttributes directly to fix lint error

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -3,26 +3,26 @@
 import * as React from "react";
 import { cn } from "@/lib/utils/cn";
 
-export interface SelectProps
-  extends React.SelectHTMLAttributes<HTMLSelectElement> {}
+// accepting all default HTML select attributes.
+const Select = React.forwardRef<
+  HTMLSelectElement,
+  React.SelectHTMLAttributes<HTMLSelectElement>
+>(({ className, children, onChange, ...props }, ref) => {
+  return (
+    <select
+      className={cn(
+        "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        className
+      )}
+      ref={ref}
+      onChange={onChange}
+      {...props}
+    >
+      {children}
+    </select>
+  );
+});
 
-const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
-  ({ className, children, onChange, ...props }, ref) => {
-    return (
-      <select
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        onChange={onChange}
-        {...props}
-      >
-        {children}
-      </select>
-    );
-  }
-);
 Select.displayName = "Select";
 
 export { Select };
